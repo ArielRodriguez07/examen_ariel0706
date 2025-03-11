8562adf96d623a9916506fcf954b664f95839647
ARIEL_RODRIGUEZ_CENICEROS <a.22308051280706@cbtis128.edu.mx>
2025-03-11 15:13:54 +0000

    arieeeeeel

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'ListView List',
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('arielrodriguez0706'),
+        backgroundColor: Color(0xffe1a5ec), // Color morado para la AppBar
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: ListView.separated(
+        separatorBuilder: (context, index) => Divider(),
+        itemCount: 7,
+        itemBuilder: (context, index) {
+          IconData icon;
+          String text;
+          switch (index) {
+            case 0:
+              icon = Icons.directions_car;
+              text = 'This is a Car, because its a car. So, it\'s a car.';
+              break;
+            case 1:
+              icon = Icons.directions_bike;
+              text =
+                  'This is a Bicycle, because its a Bicycle. So, it\'s a Bicycle.';
+              break;
+            case 2:
+              icon = Icons.directions_boat;
+              text = 'This is a Boat, because its a Boat. So, it\'s a Boat.';
+              break;
+            case 3:
+              icon = Icons.directions_bus;
+              text = 'This is a Bus, because its a Bus. So, it\'s a Bus.';
+              break;
+            case 4:
+              icon = Icons.train;
+              text = 'This is a Train, because its a Train. So, it\'s a Train.';
+              break;
+            case 5:
+              icon = Icons.directions_walk;
+              text = 'This is a Walk, because its a Walk. So, it\'s a Walk.';
+              break;
+            case 6:
+            default:
+              icon = Icons.directions_car;
+              text = 'This is a Car, because its a car. So, it\'s a car.';
+              break;
+          }
+
+          return ListTile(
+            leading: Icon(icon, size: 40),
+            title: Text(
+              text,
+              style: TextStyle(fontSize: 20),
+            ),
+          );
+        },
       ),
     );
   }
